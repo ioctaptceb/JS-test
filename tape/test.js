04129535821644e1031cf253083b893c35e419e3
ioctaptceb <yulia.startsev@gmail.com>
2016-02-21 16:53:54 +0100

    Change tape test suite to use grouping
    nested tests are possible with tape, by nesting test instances

--- tape/test.js
+++ tape/test.js
@@ -1,44 +1,48 @@
 var test = require('tape');
 var Gmtry = require('../app/main')
 
-test('Point class exists', function (t) {
-  t.equal(typeof Gmtry.Point, 'function');
-  t.end();
-});
-
-test('Point class has a shift method', function (t) {
-  t.equal(typeof Gmtry.Point.prototype.shift, 'function');
-  t.end();
-});
-
-test('A point is instantiated', function (t) {
-  var point = new Gmtry.Point(0, 0);
-  t.equal(typeof point, 'object');
-  t.end();
-});
-
-test('A point has an X coordinate', function (t) {
-    var point = new Gmtry.Point(50, 0);
-    t.equal(point.x, 50);
+test('Point', function (suite) {
+  suite.test('Point exists', function (t) {
+    t.equal(typeof Gmtry.Point, 'function');
     t.end();
+  });
+
+  suite.test('Point prototype has a shift method', function (t) {
+    t.equal(typeof Gmtry.Point.prototype.shift, 'function');
+    t.end();
+  });
 });
 
-test('A point as a Y coordinate', function (t) {
-  var point = new Gmtry.Point(0, 50);
-  t.equal(point.y, 50);
-  t.end();
-});
+test('A point', function(suite) {
+  suite.test('A point is instantiated', function (t) {
+    var point = new Gmtry.Point(0, 0);
+    t.equal(typeof point, 'object');
+    t.end();
+  });
 
-test('A point can be shifted on the x axis', function (t) {
-  var point = new Gmtry.Point(0, 50);
-  var shiftedPoint = point.shift(50, 0);
-  t.equal(shiftedPoint.x, 50);
-  t.end();
-});
+  suite.test('A point has an X coordinate', function (t) {
+      var point = new Gmtry.Point(50, 0);
+      t.equal(point.x, 50);
+      t.end();
+  });
 
-test('A point can be shifted on the y axis', function (t) {
-  var point = new Gmtry.Point(50, 150);
-  var shiftedPoint = point.shift(0, -50);
-  t.equal(shiftedPoint.y, 100);
-  t.end();
+  suite.test('A point as a Y coordinate', function (t) {
+    var point = new Gmtry.Point(0, 50);
+    t.equal(point.y, 50);
+    t.end();
+  });
+
+  suite.test('A point can be shifted on the x axis', function (t) {
+    var point = new Gmtry.Point(0, 50);
+    var shiftedPoint = point.shift(50, 0);
+    t.equal(shiftedPoint.x, 50);
+    t.end();
+  });
+
+  suite.test('A point can be shifted on the y axis', function (t) {
+    var point = new Gmtry.Point(50, 150);
+    var shiftedPoint = point.shift(0, -50);
+    t.equal(shiftedPoint.y, 100);
+    t.end();
+  });
 });
